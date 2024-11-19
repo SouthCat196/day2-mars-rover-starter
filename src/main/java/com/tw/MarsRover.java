8374619270d6892c389b9c14f2e3afe0c7dcf2e8
Sylas Qiu <qiusy@oocl.com>
2024-11-19 16:47:34 +0800

    [MarsRover] feat: add show status function

--- src/main/java/com/tw/MarsRover.java
+++ src/main/java/com/tw/MarsRover.java
@@ -1,4 +1,18 @@
 package com.tw;
 
 public class MarsRover {
+
+    private int abscissa;
+    private int ordinate;
+    private String direction;
+
+    public MarsRover() {
+        abscissa = 0;
+        ordinate = 0;
+        direction = "N";
+    }
+
+    public String showStatus() {
+        return String.format("%s:%s:%s", abscissa, ordinate, direction);
+    }
 }
