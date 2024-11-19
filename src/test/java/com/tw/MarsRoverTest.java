d09464a280048624277f889f59d66c06daadf351
Sylas Qiu <qiusy@oocl.com>
2024-11-19 17:07:22 +0800

    [MarsRover] test: should face east when execute command r given 0 0 N

--- src/test/java/com/tw/MarsRoverTest.java
+++ src/test/java/com/tw/MarsRoverTest.java
@@ -26,4 +26,11 @@ public class MarsRoverTest {
         assertEquals("0:0:W", report);
     }
 
+    @Test
+    void should_face_east_when_execute_command_r_given_0_0_N(){
+        MarsRover marsRover = new MarsRover();
+        String report = marsRover.executeCommand("R");
+        assertEquals("0:0:E", report);
+    }
+
 }
