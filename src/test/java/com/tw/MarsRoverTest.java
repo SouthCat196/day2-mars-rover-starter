c5b334c0730b7cc505a104ffec63e485873bdc4c
Sylas Qiu <qiusy@oocl.com>
2024-11-19 16:45:46 +0800

    [MarsRover] test: should show status

--- src/test/java/com/tw/MarsRoverTest.java
+++ src/test/java/com/tw/MarsRoverTest.java
@@ -2,7 +2,7 @@ package com.tw;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class MarsRoverTest {
 
@@ -10,6 +10,13 @@ public class MarsRoverTest {
     void should_initialize_mars_rover() {
         MarsRover marsRover = new MarsRover();
         assertNotNull(marsRover);
+    }
+
+    @Test
+    void should_show_status() {
+        MarsRover marsRover = new MarsRover();
+        String status = marsRover.showStatus();
+        assertEquals("0:0:N", status);
     }
 
 }
